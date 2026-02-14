48e8ed6c8cb809ecee0efae2ec3443afaa04ea30
Avin Shama <avin1991@gmail.com>
2026-02-14 18:04:16 +0530

    initial commit

---
+++ src/components/Posts.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useRef, useState } from "react";
+import { deletePost, getPost } from "../api/PostApi";
+import "../App.css";
+import Form from "./Form";
+
+export const Posts = () =>{
+
+  const [data,setData] = useState([]);
+  const [updateDataApi, setUpdateDataApi] = useState({});
+  const hasFetched = useRef(false);
+    
+  const getPostData = async () =>{
+    const res = await getPost();
+    console.log(res.data);
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+
+    getPostData();
+  }, []);
+
+  // function to delete post
+  const handleDeletePost = async (id) =>{
+    try{
+      const res = await deletePost(id);
+      if(res.status === 200)
+      {
+        const newUpdatedPosts = data.filter((curPost) => {
+            return curPost.id !== id;
+        });
+        setData(newUpdatedPosts);
+      } else{
+        console.log("Failed to delete the post:", res.status);
+      }
+    }catch (error) {
+      console.log(error);
+    }
+  };
+
+  // function to update post
+  const handleUpdatePost = (curElem) => setUpdateDataApi(curElem);
+
+  return (
+    <>
+    <section className="section-form">
+      <Form 
+      data={data} 
+      setData={setData} 
+      updateDataApi={updateDataApi} 
+      setUpdateDataApi={setUpdateDataApi} />
+    </section>
+
+    <section className="section-post">
+    <ol>
+        {
+            data.map((curElem) => {
+                const {id, body, title} = curElem;
+                return(
+                    <li key={id}>
+                        <p>Title: {title}</p>
+                        <p>Body: {body}</p>
+                        <button onClick={() => handleUpdatePost(curElem)}>Edit</button>
+                        <button className="btn-delete" onClick={() => handleDeletePost(id)}>Delete</button>
+                    </li>
+            );
+            })
+        }
+    </ol>
+  </section>
+  </>
+  );
+};
